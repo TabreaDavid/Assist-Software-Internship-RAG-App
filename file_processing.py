08f9e75ec5e405a5fb60ad3c8a8133c50c218ea8
David <ciprian.tabrea@stud.ubbcluj.ro>
2025-07-22 14:17:57 +0300

    Document processing logic

---
+++ file_processing.py
@@ -0,0 +1,23 @@
+
+import pandas
+import io
+import PyPDF2
+
+def process_pdf(content: bytes):
+    pdf_reader = PyPDF2.PdfReader(io.BytesIO(content))
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
+
+def process_xml(content: bytes):
+    return content.decode('UTF-8')
+
+def process_csv(content: bytes):
+    csv_string = content.decode('UTF-8')
+    csv_io = io.StringIO(csv_string)
+    result = pandas.read_csv(csv_io)
+    return result.to_string(index=False)
+
+def process_txt(content: bytes):
+    return content.decode('UTF-8')
